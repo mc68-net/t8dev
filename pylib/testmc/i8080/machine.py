5c8b96bb47ecb7d34b7650b71c1408b76829b546
Stuart Croy <stuartcroy@mac.com>
2024-03-16 10:58:31 +0000

    WIP testmc.i8080.machine: Use split8 to give B and C aliases for BC reg
    XXX fix generic version in previous commit and this should just work

--- pylib/testmc/i8080/machine.py
+++ pylib/testmc/i8080/machine.py
@@ -22,7 +22,12 @@ class Machine(GenericMachine):
     class Registers(GenericRegisters):
         machname  = 'i8080'
         registers = ( Reg('pc', 16), Reg('a'),
-            Reg('bc', 16), Reg('de', 16), Reg('hl', 16), Reg('sp', 16) )
+            Reg('bc', 16, split8=1), Reg('de', 16, split8=1),
+            Reg('hl', 16, split8=1), Reg('sp', 16) )
+           #    Eventually split8 etc. can be implemented like...
+           #AliasNarrow('b', 'bc', 8, 0xFF00),
+           #AliasNarrow('c', 'bc', 0, 0xFF),
+           #AliasCombine('d', ['a','b']),   # 6809 D register
         srbits    = ( Flag('S'), Flag('Z'), Bit(0), Flag('H'),
                       Bit(0), Flag('P'), Bit(0), Flag('C') )
         srname    = 'f'     # Flags Register
