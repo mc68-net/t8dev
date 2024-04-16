53c692571947566480f60eeb750ff93311364ac1
Curt J. Sampson <cjs@cynic.net>
2024-04-16 17:44:59 +0900

    WIP testmc.i8080.machine: Add disasm() to disassemble instructions

--- psrc/testmc/i8080/machine.py
+++ psrc/testmc/i8080/machine.py
@@ -1,3 +1,5 @@
+from    itertools  import chain
+
 from    testmc.generic  import *
 from    testmc.i8080.opcodes  import OPCODES, Instructions as I
 from    testmc.i8080.opimpl  import (
@@ -61,3 +63,45 @@ class Machine(GenericMachine):
     def getretaddr(self):
         return self.word(self.sp+1)
 
+    ####################################################################
+    #   Tracing and similar information
+
+    def disasm(self):
+        ''' Disassemble, at the PC, an 8080 opcode and its operands using
+            Z80 mnemonics.
+
+            This is a pretty quick hack that has not been fully tested.
+            It's probably possible to produce nicer output by taking apart
+            the mnemonics, e.g., ``LDbc`` → ``ld b,c`` etc.
+
+            TODO: Make this check `self.symtab` for addresses and,
+            if present there, print them as symbols?
+        '''
+        pc = self.regs.pc
+        op = self.byte(pc)
+        mnemonic, _ = OPCODES[op]
+        if mnemonic is None:
+            return f'DB {op:02X}'
+        if op in self.OPERAND_WORD:
+            pc1 = incword(self.regs.pc, 1)
+            pc2 = incword(self.regs.pc, 2)
+            return f'{mnemonic} {self.mem[pc2] * 0x100 + self.mem[pc1]:04X}'
+        if op in self.OPERAND_BYTE:
+            return f'{mnemonic} {self.mem[incword(self.regs.pc, 1)]:02X}'
+        return mnemonic
+
+    #   Opcodes that take no operand
+    OPERAND_WORD = frozenset(chain(
+        (0x01, 0x11, 0x21, 0x31),   # ld rr,d16
+        (0x09, 0x19, 0x29, 0x39),   # add hl,rr
+        (0x03, 0x13, 0x23, 0x33),   # inc rr
+        (0x0B, 0x1B, 0x2B, 0x3B),   # dec rr
+        (0x0A, 0x1A, 0x02, 0x12),   # lda a,(rr); ld (rr),a
+        (0xC3, 0xDA, 0xD2, 0xCA, 0xC2, 0xF2, 0xFA, 0xEA, 0xE2), # jp
+        (0xCD, 0xDC, 0xD4, 0xCC, 0xC4, 0xF4, 0xFC, 0xEC, 0xE4), # call
+        ))
+
+    OPERAND_BYTE = frozenset(chain(
+        (0x06, 0x0E, 0x16, 0x1E, 0x26, 0x2E, 0x36, 0x3E),   # ld r,d8
+        (0xC6, 0xCE, 0xD6, 0xDE, 0xE6, 0xEE, 0xF6, 0xFE),   # add r,d8 etc.
+        ))
