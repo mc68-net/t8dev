d9d32c5ef30e966b10713440752e52064441938c
Curt J. Sampson <cjs@cynic.net>
2024-04-21 12:45:13 +0900

    WIP testmc.i8080: Fix H in 8-bit inc dec, but Supersoft still failing

--- psrc/testmc/i8080/opimpl.py
+++ psrc/testmc/i8080/opimpl.py
@@ -243,19 +243,19 @@ def incSZPH(m, val):
 
 def inc_r(m, reg):
     val = incbyte(getattr(m, reg), 1)
-    setattr(m, reg, logicF(m, val, 0))
+    setattr(m, reg, logicF(m, val, H=(val & 0xF) == 0x0))
 
 def dec_r(m, reg):
     val = incbyte(getattr(m, reg), -1)
-    setattr(m, reg, logicF(m, val, 0))
+    setattr(m, reg, logicF(m, val, H=(val & 0xF) == 0xF))
 
 def inc_m(m):
     val = incbyte(m.mem[m.hl], 1)
-    m.mem[m.hl] = logicF(m, val, (val & 0xF) == 0x0)
+    m.mem[m.hl] = logicF(m, val, H=(val & 0xF) == 0x0)
 
 def dec_m(m):
     val = incbyte(m.mem[m.hl], -1)
-    m.mem[m.hl] = logicF(m, val, (val & 0xF) == 0xF)
+    m.mem[m.hl] = logicF(m, val, H=(val & 0xF) == 0xF)
 
 def inx_r(m, reg):
     setattr(m, reg, incword(getattr(m, reg),  1))
